7dc2f159f3751e7bf5a3848e9119fb383ed01056
dirtydenny <dallen7@socal.rr.com>
2023-03-28 15:27:03 -0700

    testing

--- db/index.js
+++ db/index.js
@@ -5,11 +5,12 @@ class DB {
   //   this.connection = connection;
   findAllEmployees() {
     retrun this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_title, employee.department, employee.salary, "
-    )
+      "SELECT employee.id, employee.first_name, employee.last_name, employee.role_title, employee.department, employee.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM emplyee LEFT JOIN role on employee.role_id =role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager.id;"
+    );
   }
 
 
 
 
-}
+
+} 
