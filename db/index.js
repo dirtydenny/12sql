b7e37a9c05ce5ff35eb3a69d467c83b13ca2c6cc
dirtydenny <dallen7@socal.rr.com>
2023-03-28 20:27:40 -0700

    update js

--- db/index.js
+++ db/index.js
@@ -8,9 +8,60 @@ class DB {
       "SELECT employee.id, employee.first_name, employee.last_name, employee.role_title, employee.department, employee.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM emplyee LEFT JOIN role on employee.role_id =role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager.id;"
     );
   }
+  // create, update and delete employee
+  createEmployee(employee) {
+    return this.comnection.promise().query(
+      "INSERT INTO employee SET ?", employee
+      );
+  }
+  updateEmployeeRole(employeeId, roleId) {
+    return this.connection.promise().query(
+      "UPDATE employee SET role_id = ? WHERE is = ?",
+      [roleId, employeeId]
+    )
+  }
+  removeEmployee(employee) {
+    return this.connection.promise().query(
+      "DELETE FROM employee WHERE id = ?",
+      employeeId
+      );
+    }
+    // view, create and delete department
+  viewAllDepartments() {
+      return this.connection.promise().query(
+        "SELECT department.id, department.name FROM department"
+      );
+    }
+  createDepartment(department) {
+    return this.connection.promise().query(
+      "INSERT INTO department SET ?",
+      department
+      );
+  }
+  removeDepartment(departmentId) {
+    return this.connection.promise().query(
+      "DELETE FROM department WHERE id = ?",
+      departmentId
+      );
+  }
+  //Add and delete roles
+  findAllRoles() {
+    return this.connection.promise().query(
+      "SEELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT Join department on role.department_id = department.id;"
+    )
+  }
 
-
-
-
-
+  createRole(role) {
+    return this.connection.promise().query(
+      "INSERT INTO role SET ?",
+      role
+    );
+  }
+  removeRole(roleId) {
+    return this.connection.promise().query(
+      "DELETE FROM role WHERE id = ?",
+      roleId
+      );
+    }
 } 
+module.exports = new DB(connection);
--- server.js
+++ server.js
@@ -15,15 +15,7 @@ const menuOptions = () => {
           {
             name: "View All Employees",
             value: "VIEW_ALL_EMPLOYEES"
-          },
-          // {
-          //   name: "View All Employees By Department",
-          //   value: "VIEW_ALL_EMPLOYEES_BY_DEPARTMENT"
-          // },
-          // {
-          //   name: "View All Employees By Manager",
-          //   value: "VIEW_EMPLOYEES_BY_MANAGER"
-          // },          
+          },          
           {
             name: "Add Employee",
             value: "ADD_EMPLOYEE"
@@ -40,10 +32,10 @@ const menuOptions = () => {
             name: "Update Employee Role",
             value: "UPDATE_EMPLOYEE_ROLE"
           },
-          // {
-          //   name: "Update Employee Manager",
-          //   value: "UPDATE_EMPLOYEE_MANAGER"
-          // },
+          {
+            name: "Update Employee Manager",
+            value: "UPDATE_EMPLOYEE_MANAGER"
+          },
           {
             name: "Add Role",
             value: "ADD_ROLE"
@@ -55,11 +47,7 @@ const menuOptions = () => {
           {
             name: "View All Departments",
             value: "VIEW_ALL_DEPARTMENTS"
-          },
-          // {
-          //   name: "View Total Utilized Budget By Department",
-          //   value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT"
-          // },
+          },          
           {
             name: "Quit",
             value: "QUIT",
@@ -73,35 +61,173 @@ const menuOptions = () => {
           viewAllEmployees();
           break;
         case "ADD_EMPLOYEE":
-          addEmployee():
+          addEmployee();
           break;
         case "REMOVE_EMPLOYEE":
-          removeEmployee():
+          removeEmployee();
           break;  
         case "VIEW_ALL_ROLES":
-          viewAllRoles():
+          viewAllRoles();
           break;  
         case "UPDATE_EMPLOYEE_ROLE":
-          updateEmployeeRole():
+          updateEmployeeRole();
           break;
         case "ADD_ROLE":
-          addRole():
+          addRole();
           break;
         case "REMOVER_ROLE":
-          removeRole():
+          removeRole();
           break;
         case "VIEW_ALL_DEPARTMENTS":
-          viewAllDepartments():
+          viewAllDepartments();
           break;
       }
     })
 }
-function viewAllEmployees() {
-  db.findAllEmployees()
-  .then(([rows]) => {
-    let employees = rows;
+// view all employees
+
+async function viewAllEmployees() {
+  try {
+    const [rows] = await db.findAllEmployees();
+    const employees = rows;
     console.log("\n");
     console.table(employees);
-  })
-  .then(() => menuOptions());
+    await menuOptions();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// update employee role
+async function updateEmployeeRole() {
+  try {
+    const [employeeRows] = await db.findAllEmployees();
+    const employees = employeeRows;
+    const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id
+    }));
+
+    const { employeeId } = await prompt([
+      {
+        type: "list",
+        name: "employeeId",
+        message: "Who's role do you want to update?",
+        choices: employeeChoices
+      }
+    ]);
+
+    const [roleRows] = await db.findAllRoles();
+    const roles = roleRows;
+    const roleChoices = roles.map(({ id, title }) => ({
+      name: title,
+      value: id
+    }));
+
+    const { roleId } = await prompt([
+      {
+        type: "list",
+        name: "roleId",
+        message: "What is the employee's updated role?",
+        choices: roleChoices
+      }
+    ]);
+
+    await db.updateEmployeeRole(employeeId, roleId);
+    console.log("Updated employee's role");
+    await menuOptions();
+  } catch (error) {
+    console.error(error);
+  }
+}
+// view roles
+async function viewAllRoles() {
+  try {
+    const [rows] = await db.viewAllRoles();
+    const roles = rows;
+    console.log("\n");
+    console.table(roles);
+    await menuOptions();
+  } catch (error) {
+    console.error(error);
+  }
+}
+  
+// Add employee
+async function addEmployee() {
+  try {
+    const employee = await prompt([
+      {
+        name: "first_name",
+        message: "What is the employee's first name?",
+      },
+      {
+        name: "last_name",
+        message: "What is the employee's last name?",
+      },
+    ]);
+
+    const roles = await db.findAllRoles();
+    const roleChoices = roles[0].map(({ id, title }) => ({
+      name: title,
+      value: id,
+    }));
+
+    const { roleId } = await prompt({
+      type: "list",
+      name: "roleId",
+      message: "What is the employee's role?",
+      choices: roleChoices,
+    });
+
+    const employees = await db.findAllEmployees();
+    const managerChoices = employees[0].map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id,
+    }));
+
+    managerChoices.unshift({ name: "None", value: null });
+
+    const { managerId } = await prompt({
+      type: "list",
+      name: "managerId",
+      message: "Who is the employee's manager?",
+      choices: managerChoices,
+    });
+
+    const newEmployee = {
+      manager_id: managerId,
+      role_id: roleId,
+      first_name: employee.first_name,
+      last_name: employee.last_name,
+    };
+
+    await db.createEmployee(newEmployee);
+    console.log(`Added ${newEmployee.first_name} ${newEmployee.last_name} to the database`);
+    await menuOptions();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addDepartment() {
+  try {
+    const department = await prompt([
+      {
+        name: "name",
+        message: "New department's name?",
+      },
+    ]);
+    const name = res.name;
+    await db.createDepartment(name);
+    console.log(`Added ${name} to the database`);
+    await menuOptions();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function quit(){
+  console.log("Back to Work!");
+  process.exit();
 }
